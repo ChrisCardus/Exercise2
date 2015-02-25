533932a144e103d9fbe8d3f4cb6ec41def35f588
Chris Cardus <cxc357@student.bham.ac.uk>
2015-02-25 13:02:29 +0000

    Completed part 2

--- src/rp/Robot.java
+++ src/rp/Robot.java
@@ -9,9 +9,14 @@ import lejos.robotics.navigation.DifferentialPilot;
 
 public class Robot {
 	
+	public final static int STRAIGHT = 0;
+	public final static int RIGHT = 1;
+	public final static int LEFT = 2;
 	private DifferentialPilot pilot;
 	private LightSensor lightRight;
 	private LightSensor lightLeft;
+	private int blackLeftInit;
+	private int blackRightInit;
 	private RangeFinder ultra;
 	private int leftInit;
 	private int rightInit;
@@ -30,6 +35,11 @@ public class Robot {
 	public void setInit(int l, int r) {
 		this.leftInit = l;
 		this.rightInit = r;
+	}
+	
+	public void setBlackInit(int l, int r) {
+		this.blackLeftInit = l;
+		this.blackRightInit = r;
 	}
 	
 	public int getLeftInit() {
@@ -73,6 +83,10 @@ public class Robot {
 			pilot.setTravelSpeed(-travelSpeed);
 			pilot.backward();
 		}
+	}
+	
+	public void travel(int distance){
+		pilot.travel(distance);
 	}
 	
 	public void followLine(){
@@ -120,7 +134,7 @@ public class Robot {
 		lightLeft.setFloodlight(true);
 	}
 
-	public void turn(boolean left) {
+	public void turn(int direction) {
 		/*if(getRange() < 5){
 			edge = true;
 			direction = left;
@@ -129,12 +143,13 @@ public class Robot {
 		//remember if turned left or turned right (when you approached an edge)
 		//if turned left turning left again sets edge to false
 		//if turned right turning right again set edge to false
+		
 		pilot.travel(3);
 		
-		if(left){
-			pilot.rotate(90);
-		} else {
+		if(direction == 2){
 			pilot.rotate(-90);
+		} else if(direction == 1) {
+			pilot.rotate(90);
 		}
 	}
 }
